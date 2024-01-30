52f0ecbd71bc3bfdf81a600e3203ab8e5334e50c
minjidev <68722909+minjidev@users.noreply.github.com>
2024-01-30 15:29:55 +0900

    Title: 대문자로 바꾸기, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

--- 240130/대문자로 바꾸기/change-to-capital.js
+++ 240130/대문자로 바꾸기/change-to-capital.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim().split('\n')
-const arr = input.map(row => row.split(' '))
+const arr = input.map(row => row.trim().split(' '))
 
 for (let i=0;i<arr.length;i++) {
     for (let j=0;j<arr[0].length;j++) {
-        arr[i][j] = arr[i][j].toUpperCase()
+        arr[i][j] = (arr[i][j]).toUpperCase()
     }
 }
 console.log(arr.map(row => row.join(' ')).join('\n'))
