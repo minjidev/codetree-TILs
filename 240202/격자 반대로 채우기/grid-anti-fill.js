49efd602340e5b72521b1c7707292ccb40721f2d
minjidev <68722909+minjidev@users.noreply.github.com>
2024-02-02 23:54:47 +0900

    Title: 격자 반대로 채우기, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240202/격자 반대로 채우기/grid-anti-fill.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const N = +fs.readFileSync(0).toString().trim().split(' ')
+const answer = Array.from({ length: N }, () => Array(N).fill(0))
+
+let num = 0
+for (let i=N-1;i>=0;i--) {
+    if (i % 2 === 1) {
+        for (let j=N-1;j>=0;j--) {
+            num += 1
+            answer[j][i] = num
+        }
+    } else {
+        for (let j=0;j<N;j++) {
+            num += 1
+            answer[j][i] = num
+        }
+    }
+   
+}
+
+console.log(answer.map(row => row.join(' ')).join('\n'))
