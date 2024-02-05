62a26178eab0a182ad29a765ccf21e859e0c1612
minjidev <68722909+minjidev@users.noreply.github.com>
2024-02-05 18:58:40 +0900

    Title: 격자 모양 출력하기, Time: 227ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240205/격자 모양 출력하기/print-grid-shape.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const [nums, ...input] = fs.readFileSync(0).toString().trim().split('\n')
+
+const [N, M] = nums.split(' ').map(Number)
+const arr = input.map(row => row.split(' ').map(Number))
+const answer = Array.from({ length: N }, () => Array(N).fill(0))
+
+for (let [x, y] of arr) {
+    answer[x-1][y-1] = x * y
+}
+
+console.log(answer.map(row => row.join(' ')).join('\n'))
