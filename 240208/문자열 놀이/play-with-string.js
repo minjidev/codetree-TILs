670246be47b8fe6a16f869bdf850c8a1fcd8e8f1
minjidev <68722909+minjidev@users.noreply.github.com>
2024-02-08 11:03:13 +0900

    Title: 문자열 놀이, Time: 167ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240208/문자열 놀이/play-with-string.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split('\n')
+const [str, n] = input[0].split(' ')
+const arr = str.split('')
+const query = input.slice(1).map(row => row.split(' ').map((e) => isNaN(e) ? e : +e))
+
+for (let [type, a, b] of query) {
+    if (type === 1) {
+        for (let i=0;i<arr.length;i++) {
+            [arr[a-1], arr[b-1]] = [arr[b-1], arr[a-1]]
+        }
+    } else {
+        for (let i=0;i<arr.length;i++) {
+            if (arr[i] === a) {
+                arr[i] = b
+            }
+        }
+    }
+    console.log(arr.join(''))
+}
