c7c210b8467369ce8d8297bfd09f7c6fa7b97ba6
minjidev <68722909+minjidev@users.noreply.github.com>
2024-02-12 11:00:47 +0900

    Title: 2021년 날짜의 유무, Time: 190ms, Memory: 14MB, Status: Passed - CodeTree

--- 240212/2021년 날짜의 유무/with-or-without-2021.js
+++ 240212/2021년 날짜의 유무/with-or-without-2021.js
@@ -3,6 +3,7 @@ const [m, d] = fs.readFileSync(0).toString().trim().split(' ').map(Number)
 const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 function checkExist(m, d) {
+    if (m > 12) return 'No'
     if (days[m-1] < d) return 'No'
     return 'Yes'
 }
