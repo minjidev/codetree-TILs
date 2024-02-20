7d5b845e2df8e50447cd3b0a01430b223a4d592c
minjidev <68722909+minjidev@users.noreply.github.com>
2024-02-20 09:13:03 +0900

    Title: 금 채굴하기, Time: 181ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 240220/금 채굴하기/gold-mining.js
+++ 240220/금 채굴하기/gold-mining.js
@@ -2,20 +2,37 @@ const fs = require('fs')
 const [nums, ...arr] = fs.readFileSync(0).toString().trim().split('\n')
 const [N, M] = nums.split(' ').map(Number)
 const board = arr.map(row => row.split(' ').map(Number))
+const dir = [[1, -1], [1, 1], [-1, 1], [-1, -1]] // 4방향
 let max = 0
+
 const getCost = (k) => k * k + (k+1) * (k+1)
 
+const isOutside = (x, y) => x < 0 || y < 0 || x >= N || y >= N
+
 const getGoldCount = (row, col, k) => {
-  let goldCount = 0
-  for (let i=0;i<N;i++) {
-    for (let j=0;j<N;j++) {
-      if (Math.abs(i-row) + Math.abs(j-col) <=k) {
-        goldCount += board[i][j]
+  let goldCount = board[row][col] // 중심점
+
+  for (let i=1;i<=k;i++) {
+    // 시작점 지정
+    let x = row - i
+    let y = col
+    // 4방향 순회 
+    for (let j=0;j<4;j++) {
+      for (let l=0;l<k;l++) {
+        if (isOutside(x, y)) continue
+
+        // 금 채굴 
+        goldCount += board[x][y]
+
+        // 이동 
+        x += dir[j][0]
+        y += dir[j][1]
       }
     }
   }
 
   return goldCount
+
 }
 
 for (let i=0;i<N;i++) {
@@ -24,9 +41,10 @@ for (let i=0;i<N;i++) {
     for (let k=0;k<2*(N-1)+1;k++) {
       const goldCount = getGoldCount(i, j, k)
       const cost = getCost(k)
-      if (goldCount * M < cost) continue
       // 손해가 아닌 경우
-      max = Math.max(max, goldCount)
+      if (cost <= goldCount * M) {
+        max = Math.max(max, goldCount)
+      }
     }
   }
 }
