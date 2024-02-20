41b98c3d1fe1adfe500ae1a5ad63e480ae313f7c
minjidev <68722909+minjidev@users.noreply.github.com>
2024-02-20 10:02:55 +0900

    Title: 기울어진 직사각형, Time: 188ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 240220/기울어진 직사각형/slanted-rectangle.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const [n, ...arr] = fs.readFileSync(0).toString().trim().split('\n')
+
+const N = +n
+const board = arr.map(row => row.split(' ').map(Number))
+const dir = [[-1, 1], [-1, -1], [1, -1], [1, 1]]
+let maxSum = 0
+
+const isOutside = (x, y) => {
+    return x < 0 || y < 0 || x >= N || y >= N
+}
+
+const getSum = (row, col, k) => {
+    if (k === 0) {
+        return board[row][col]
+    }
+
+    let sum = 0
+    // 시작점 
+    let x = row + k
+    let y = col
+
+    for (let i=0;i<4;i++) {
+        for (let j=0;j<k;j++) {
+            // 이동하다 범위 벗어나면 중단 
+            if (isOutside(x, y)) {
+                return 0
+            }
+
+            sum += board[x][y]
+
+            x += dir[i][0]
+            y += dir[i][1]
+        }
+    }
+
+    return sum 
+}
+
+for (let i=0;i<N;i++) {
+    for (let j=0;j<N;j++) {
+        for (k=0;k<2*(N-1);k++) {
+            // 각 좌표에서 k일 때 합 구하기 
+            maxSum = Math.max(maxSum, getSum(i, j, k))
+        }
+    }
+}
+
+console.log(maxSum)
