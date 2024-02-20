b847bc5aa19224d009625b186e19e1f2ff748940
minjidev <68722909+minjidev@users.noreply.github.com>
2024-02-20 12:40:25 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 206ms, Memory: 47MB, Status: Passed - CodeTree

--- 240220/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.js
+++ 240220/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.js
@@ -6,32 +6,30 @@ const board = arr.map(row => row.split(' ').map(Number))
 let maxSum = Number.MIN_SAFE_INTEGER
 
 
-const getSum = (x, y, w, h) => {
+const getSum = (x1, y1, x2, y2) => {
     let sum = 0
 
-    for (let i=x;i<=x+h;i++) {
-        for (let j=y;j<=y+w;j++) {
+    for (let i=x1;i<=x2;i++) {
+        for (let j=y1;j<=y2;j++) {
             sum += board[i][j]
         }
     }
-    
+
     return sum
 }
 
-const getMaxSum = (x, y, w1, h1) => {
-    const sum = getSum(x, y, w1, h1)
+const getMaxSum = (x1, y1, x2, y2) => {
+    const sum = getSum(x1, y1, x2, y2)
     let maxSum = Number.MIN_SAFE_INTEGER
 
     // 두 번째 사각형 만들기 
     for (let i=0;i<N;i++) {
         for (let j=0;j<M;j++) {
-            for (let w2=0;w2<M;w2++) {
-                for (let h2=0;h2<N;h2++) {
-                    // 가로 세로 끝이 범위를 벗어나면 무시 
-                    if (i + h2 >= N || j + w2 >= M) continue
+            for (let k=i;k<N;k++) {
+                for (let l=j;l<M;l++) {
                     // 사각형 외부라면 
-                    if (x + h1 < i || y + w1 < j) {
-                        const total = sum + getSum(i, j, w2, h2)
+                    if (x2 < i || y2 < j) {
+                        const total = sum + getSum(i, j, k, l)
                         maxSum = Math.max(maxSum, total)
                     }
                    
@@ -44,15 +42,12 @@ const getMaxSum = (x, y, w1, h1) => {
 
 }
 
-// 첫 번째 직사각형 
+// 첫 번째 직사각형 (i, j), (k, l)을 꼭지점으로
 for (let i=0;i<N;i++) {
     for (let j=0;j<M;j++) {
-        // 직사각형 가로, 세로 최대 
-        for (let w=0;w<M;w++) {
-            for (let h=0;h<N;h++) {
-                // 만약 가로 세로 끝이 범위를 벗어나면 무시 
-                if (i + h >= N || j + w >= M) continue
-                maxSum = Math.max(maxSum, getMaxSum(i, j, w, h))
+        for (let k=i;k<N;k++) {
+            for (let l=j;l<M;l++) {
+                maxSum = Math.max(maxSum, getMaxSum(i, j, k, l))
             }
         }
     }
