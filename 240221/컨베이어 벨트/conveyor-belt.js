399b7e7e78d913c09860241fcdb15bbb47f9d5c9
minjidev <68722909+minjidev@users.noreply.github.com>
2024-02-21 10:41:38 +0900

    Title: 컨베이어 벨트, Time: 250ms, Memory: 15MB, Status: Passed - CodeTree

--- 240221/컨베이어 벨트/conveyor-belt.js
+++ 240221/컨베이어 벨트/conveyor-belt.js
@@ -1,21 +1,25 @@
 const fs = require('fs')
 const [nums, ...arr] = fs.readFileSync(0).toString().trim().split('\n')
-
 const [N, T] = nums.split(' ').map(Number)
-const board = arr.map(row => row.split(' ').map(Number))
+const board = arr.map(row => row.trim().split(' ').map(Number))
+board[1].reverse()
+
 
 for (let i=0;i<T;i++) {
-    const firstLast = board[0][N-1]
-    const secondLast = board[1][N-1]
+    const last = board[0][N-1]
+    const first = board[1][0]
 
-    // 밀기 
     for (let i=N-1;i>=1;i--) {
         board[0][i] = board[0][i-1]
-        board[1][i] = board[1][i-1]
     }
 
-    board[0][0] = secondLast
-    board[1][0] = firstLast
+    for (let i=0;i<N-1;i++) {
+        board[1][i] = board[1][i+1]
+    }
+
+    board[0][0] = first
+    board[1][N-1] = last
 }
 
+board[1].reverse()
 console.log(board.map(row => row.join(' ')).join('\n'))
