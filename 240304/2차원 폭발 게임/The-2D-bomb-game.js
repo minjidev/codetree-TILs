020654cc21f47077947607e4fc88d6607386f69f
minjidev <68722909+minjidev@users.noreply.github.com>
2024-03-04 19:46:43 +0900

    Title: 2차원 폭발 게임, Time: 382ms, Memory: 22MB, Status: Wrong Answer - CodeTree

---
+++ 240304/2차원 폭발 게임/The-2D-bomb-game.js
@@ -0,0 +1,126 @@
+const fs = require('fs')
+const [nums, ...arr] = fs.readFileSync(0).toString().trim().split('\n')
+
+const [N, M, K] = nums.split(' ').map(Number)
+const board = arr.map(row => row.split(' ').map(Number))
+let tmp = Array.from({ length: N }, () => Array(N).fill(0))
+
+// copy
+function copy() {
+    for (let i=0;i<N;i++) {
+        for (let j=0;j<N;j++) {
+            board[i][j] = tmp[i][j]
+        }
+    }
+}
+
+function explode() {
+    // explode 
+    // 각 열의 연속하는 숫자의 마지막 index를 찾아서 (개수) >= M이면 0으로 변경 
+    for (let i=0;i<N;i++) {
+        while (true) {
+            let didExplode = false
+            for (let j=0;j<N;j++) {
+                const cur = board[j][i]
+
+                if (cur === 0) continue
+
+                const end = getLastIndexOf(j, i, cur)
+
+                // M개 이상 숫자 연속되면 폭발 
+                if (end - j + 1 >= M) {
+                    fillZero(j, end, i)
+                    didExplode = true
+                }
+                
+            }
+
+            // 더 이상 폭발할 게 없으면 중지 
+            if (!didExplode) break
+        }
+    }
+}
+
+
+// rotate 90d 
+function rotate() {
+    tmp = Array.from({ length: N }, () => Array(N).fill(0))
+    for (let i=0;i<N;i++) {
+        for (let j=0;j<N;j++) {
+            tmp[j][N-i-1] = board[i][j]
+        }
+    }
+
+    copy()
+}
+
+
+
+function getLastIndexOf(row, col, val) {
+    let end = row + 1
+    while (end < N) {
+        if (board[end][col] === val) {
+            end +=1 
+        } else {
+            break
+        }
+    }
+
+    return end - 1
+}
+
+function fillZero(start, end, col) {
+    for (let i=start;i<=end;i++) {
+        board[i][col] = 0
+    }
+}
+
+function drop() {
+    tmp = Array.from({ length: N }, () => Array(N).fill(0))
+    for (let i=0;i<N;i++) {
+        let tmpEnd = N-1
+        for (let j=N-1;j>=0;j--) {
+            const cur = board[j][i]
+            if (cur === 0) continue
+
+            tmp[tmpEnd][i] = board[j][i]
+            tmpEnd -= 1
+        }
+    }
+
+    copy()
+}
+
+function countBombs() {
+    let count = 0
+    for (let i=0;i<N;i++) {
+        for (let j=0;j<N;j++) {
+            if (board[i][j] === 0) continue
+
+            count += 1
+        }
+    }
+
+    return count
+}
+
+let rep = 0
+while (rep < K) {
+    // 폭발
+    explode()
+
+    // 중력
+    drop()
+
+    // 회전
+    rotate()
+
+    // 중력
+    drop()
+
+    rep += 1
+}
+
+explode()
+const bombCount = countBombs()
+console.log(bombCount)
