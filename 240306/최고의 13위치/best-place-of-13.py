e11302fd1a958d361c30404c9693d7089de09a19
minjidev <68722909+minjidev@users.noreply.github.com>
2024-03-06 21:39:03 +0900

    Title: 최고의 13위치, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240306/최고의 13위치/best-place-of-13.py
+++ 240306/최고의 13위치/best-place-of-13.py
@@ -4,9 +4,9 @@ max_cnt = 0
 
 for i in range(n):
     for j in range(n-2):
-        cnt = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+        cnt = (arr[i][j] + arr[i][j+1] + arr[i][j+2])
     
-    max_cnt = max(max_cnt, cnt)
+        max_cnt = max(max_cnt, cnt)
 
 
 print(max_cnt)
