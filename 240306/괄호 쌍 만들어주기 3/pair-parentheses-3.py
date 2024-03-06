ec504cfcd37cac5df182218f957dd36cd09af939
minjidev <68722909+minjidev@users.noreply.github.com>
2024-03-06 21:14:57 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240306/괄호 쌍 만들어주기 3/pair-parentheses-3.py
+++ 240306/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -1,15 +1,12 @@
 import sys
 
-arr = input()
-n = len(arr)
+string = input()
+n = len(string)
 count = 0
 
-for i in range(n):
-    if arr[i] == ')': 
-        continue
-    
+for i in range(n):    
     for j in range(i+1, n):
-        if arr[j] == ')':
+        if string[i] == '(' and string[j] == ')':
             count += 1
 
 print(count)
