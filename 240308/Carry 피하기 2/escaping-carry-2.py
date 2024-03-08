1f659beb3e234db44d564b8d980eb38beaaf9927
minjidev <68722909+minjidev@users.noreply.github.com>
2024-03-08 09:52:12 +0900

    Title: Carry 피하기 2, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240308/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,33 @@
+n = int(input())
+arr = list(int(input()) for _ in range(n))
+max_sum = 0
+
+def isCarry(a, b):
+    arr1 = list(map(int, str(a)))
+    arr2 = list(map(int, str(b)))
+
+    min_len = min(len(arr1), len(arr2))
+
+    for i in range(min_len):
+        if arr1[-1-i] + arr2[-1-i] >= 10:
+            return True
+    return False
+
+def getMaxSum(a, b, c):
+    if isCarry (a, b) or isCarry(b, c) or isCarry(a, b):
+        return 0
+    else:
+        return a + b + c
+
+
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            result = getMaxSum(arr[i], arr[j], arr[k])
+            max_sum = max(max_sum, result)
+
+if (max_sum == 0):
+    print(-1)
+else:
+    print(max_sum)
