bec84e786c59b013d23d7ce194ffec454b45565b
minjidev <68722909+minjidev@users.noreply.github.com>
2024-03-08 15:34:14 +0900

    Title: 오목, Time: 149ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240308/오목/O-mok.py
@@ -0,0 +1,49 @@
+# DFS로 8방향 연속 5개 더해지는 순간 정답 출력  
+# (행, 열) 출력 
+BOARD_LEN = 19
+DIR_LEN = 8
+board = [list(map(int, input().split())) for _ in range(BOARD_LEN)]
+dir_arr = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [1, 1], [-1, 1], [1, -1]] # 8방향 확인 
+location = []
+winner = []
+def isOutside(x, y):   
+    return x < 0 or y < 0 or x >= BOARD_LEN or y >= BOARD_LEN
+
+
+def DFS(x, y, turn, cur_dir, cnt):
+    if cnt == 5:
+        winner.append(turn)
+        location.extend([x, y])
+        return
+    
+    nx = x + dir_arr[cur_dir][0]
+    ny = y + dir_arr[cur_dir][1]
+    # 격자 외부이거나 방문한 경우 skip 
+    if isOutside(nx, ny) or board[nx][ny] != turn: return
+
+    # 같은 숫자인 경우 같은 방향으로 이동 
+    board[nx][ny] = -1
+    DFS(nx, ny, turn, cur_dir, cnt + 1)
+    board[nx][ny] = turn
+
+
+
+for i in range(BOARD_LEN):
+    for j in range(BOARD_LEN):
+        if board[i][j] == 0: 
+            continue
+        # 모든 격자에 대해 8방향 확인
+        for k in range(DIR_LEN):
+            cur = board[i][j]
+            board[i][j] = -1
+            DFS(i, j, cur, k, 1)
+            board[i][j] = cur
+
+
+print(winner[0])
+[r1, c1, r2, c2] = location
+# 열로 놓인 경우 
+if (r1 == r2):
+    print(r1+1, (c1+c2)//2+1)
+else:
+    print((r1+r2)//2+1, c1)
