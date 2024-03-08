e11a55351704028b629c9b60e51c015841053c92
minjidev <68722909+minjidev@users.noreply.github.com>
2024-03-08 16:34:00 +0900

    Title: 오목, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240308/오목/O-mok.py
+++ 240308/오목/O-mok.py
@@ -6,15 +6,18 @@ board = [list(map(int, input().split())) for _ in range(BOARD_LEN)]
 dir_arr = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [1, 1], [-1, 1], [1, -1]] # 8방향 확인 
 location = []
 winner = 0
+
 def isOutside(x, y):   
     return x < 0 or y < 0 or x >= BOARD_LEN or y >= BOARD_LEN
 
 
 def DFS(x, y, turn, cur_dir, cnt):
+    global winner
     if cnt == 5:
-        global winner
+        # 끝점 찾은 경우 
         winner = turn
-        location.extend([x, y])
+        # 현재 방향 기준으로 2번 이동 
+        location.extend([x - dir_arr[cur_dir][0] * 2 + 1 , y - dir_arr[cur_dir][1] * 2 + 1])
         return
     
     nx = x + dir_arr[cur_dir][0]
@@ -33,6 +36,8 @@ for i in range(BOARD_LEN):
     for j in range(BOARD_LEN):
         if board[i][j] == 0: 
             continue
+        if winner > 0: 
+            break
         # 모든 격자에 대해 8방향 확인
         for k in range(DIR_LEN):
             cur = board[i][j]
@@ -43,15 +48,6 @@ for i in range(BOARD_LEN):
 
 if (winner>0):
     print(winner)
-    [r1, c1, r2, c2] = location
-    # 열로 놓인 경우 
-    if r1 == r2:
-        print(r1+1, (c1+c2)//2+1)
-    # 행으로 놓인 경우
-    elif c1 == c2:
-        print((r1+r2)//2+1, c1+1) 
-    # 대각선으로 놓인 경우 
-    else:
-        print((r1+r2)//2+1, (c1+c2)//2+1)
+    print(location[0], location[1])
 else:
     print(0)
