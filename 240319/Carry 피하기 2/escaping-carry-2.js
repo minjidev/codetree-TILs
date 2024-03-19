910334d7a578a2821b3c07b506534cdc1ba1cd2f
minjidev <68722909+minjidev@users.noreply.github.com>
2024-03-19 17:39:55 +0900

    Title: Carry 피하기 2, Time: 235ms, Memory: 14MB, Status: Passed - CodeTree

--- 240319/Carry 피하기 2/escaping-carry-2.js
+++ 240319/Carry 피하기 2/escaping-carry-2.js
@@ -6,11 +6,11 @@ let maxSum = -1
 
 // isCarray(숫자 2개) => 각 자리수 더했을 때 carry 발생하면 true, 아니면 false 반환 
 function isCarry(a, b) {
-    for (let i=0;i<3;i++) {
+    for (let i=0;i<4;i++) {
         const mod = 10**(i+1) // 해당 자리수까지의 숫자 구하기
         const divider = 10**i // 해당 자리수 구하기
 
-        const sum = Math.floor(a % mod / divider) + Math.floor(b % mod / divider)
+        const sum = Math.floor((a % mod) / divider) + Math.floor((b % mod) / divider)
 
         if (sum >= 10) return true
     }
