0c5360b3745cbf90749af9144c3c3e221beb9328
minjidev <68722909+minjidev@users.noreply.github.com>
2024-03-20 08:53:17 +0900

    Title: G or H 3, Time: 223ms, Memory: 51MB, Status: Wrong Answer - CodeTree

--- 240320/G or H 3/G-or-H-3.js
+++ 240320/G or H 3/G-or-H-3.js
@@ -1,30 +1,33 @@
 const fs = require('fs')
 const [n, ...arr] = fs.readFileSync(0).toString().trim().split('\n')
-
 const [N, K] = n.split(' ').map(Number) // K+1 구간 확인
 const ppl = arr.map(row => row.split(' ').map(v => isNaN(v) ? v : Number(v)))
-const maxLen = Math.max(...ppl.map(([a, b]) => a))
-const alphabets = Array(maxLen).fill(0)
+const MAX_LEN = 10000
+const alphabets = Array(MAX_LEN).fill(0)
 let maxSum = 0
 
 for (let [i, v] of ppl) {
-    alphabets[i] = v
+    alphabets[i] = (v === 'G' ? 1 : 2)
 }
 
-const end = K < maxLen ? maxLen - K + 2 : 1
+console.log(alphabets)
 
-for (let i=0;i<end;i++) {
+
+for (let i=0;i<=MAX_LEN-K;i++) {
     let sum = 0
-    for (let j=0;j<K+1;j++) {
+    for (let j=i;j<i+K+1;j++) {
+        // console.log(alphabets[j])
+        sum += alphabets[j]
 
-        if (alphabets[i+j] === 'G') {
-            sum += 1
-        } 
+        // if (alphabets[i+j] === 'G') {
+        //     sum += 1
+        // } 
         
-        if (alphabets[i+j] === 'H') {
-            sum += 2
-        }
+        // if (alphabets[i+j] === 'H') {
+        //     sum += 2
+        // }
     }
+    // console.log(sum)
 
     maxSum = Math.max(maxSum, sum)
 }
