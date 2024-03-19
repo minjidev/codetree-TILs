a2d69a2cdb23798a84d1add2198d33fa83f64c6e
minjidev <68722909+minjidev@users.noreply.github.com>
2024-03-20 08:08:19 +0900

    Title: G or H 3, Time: 544ms, Memory: 18MB, Status: Passed - CodeTree

--- 240320/G or H 3/G-or-H-3.js
+++ 240320/G or H 3/G-or-H-3.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const [n, ...arr] = fs.readFileSync(0).toString().trim().split('\n')
 
-const [N, K] = n.split(' ').map(Number) // K+1칸 검사 필요
+const [N, K] = n.split(' ').map(Number) // K+1 구간 확인
 const ppl = arr.map(row => row.split(' ').map(v => isNaN(v) ? v : Number(v)))
 const maxLen = Math.max(...ppl.map(([a, b]) => a))
 const alphabets = Array(maxLen).fill(0)
@@ -11,9 +11,12 @@ for (let [i, v] of ppl) {
     alphabets[i] = v
 }
 
-for (let i=0;i<maxLen-K+2;i++) {
+const end = K < maxLen ? maxLen - K + 2 : 1
+
+for (let i=0;i<end;i++) {
     let sum = 0
     for (let j=0;j<K+1;j++) {
+
         if (alphabets[i+j] === 'G') {
             sum += 1
         } 
