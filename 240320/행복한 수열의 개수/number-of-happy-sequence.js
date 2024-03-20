0c8c01011f6f5f9b4235e48dee5a95be4f3d548d
minjidev <68722909+minjidev@users.noreply.github.com>
2024-03-20 19:23:42 +0900

    Title: 행복한 수열의 개수, Time: 158ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240320/행복한 수열의 개수/number-of-happy-sequence.js
@@ -0,0 +1,44 @@
+// 행, 열 확인해서 m개 연속 같은 숫자면 count 
+
+const fs = require('fs')
+const [n, ...arr] = fs.readFileSync(0).toString().trim().split('\n')
+
+const [N, M] = n.split(' ').map(Number)
+const board = arr.map(row => row.split(' ').map(Number))
+let answer = 0
+
+for (let i=0;i<N;i++) {
+    let count = 1
+    for (let j=1;j<N;j++) {
+        if (board[i][j-1] === board[i][j]) {
+            count += 1
+        } else {
+            count = 1
+        }
+
+        if (count >= M) {
+            answer += 1
+            break
+        }
+    }
+}
+
+for (let i=0;i<N;i++) {
+    let count = 1
+    for (let j=1;j<N;j++) {
+        if (board[j-1][i] === board[j][i]) {
+            count += 1
+        } else {
+            count = 1
+        }
+
+
+        if (count >= M) {
+            answer += 1
+            break
+        }
+    }
+
+}
+
+console.log(answer)
