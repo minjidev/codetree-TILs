d0af2314e48587c3e4356fa033620eed829b7cce
minjidev <68722909+minjidev@users.noreply.github.com>
2024-03-20 13:41:39 +0900

    Title: 아름다운 수열 2, Time: 230ms, Memory: 14MB, Status: Passed - CodeTree

--- 240320/아름다운 수열 2/beautiful-sequence-2.js
+++ 240320/아름다운 수열 2/beautiful-sequence-2.js
@@ -6,37 +6,31 @@ const [n, ...arr] = fs.readFileSync(0).toString().trim().split('\n')
 const [N, M] = n.split(' ').map(Number)
 const [str1, str2] = arr
 const a = str1.split(' ').map(Number)
-const b = str2.split(' ').map(Number)
-const MAX_LEN = Math.max(...[...a, ...b])
+const b = str2.split(' ').map(Number).sort()
+const MAX_LEN = 100
+
 
 let answer = 0
+let tmp = Array(MAX_LEN).fill(MAX_LEN+1)
 
 for (let i=0;i<N-M+1;i++) {
-    for (let j=i+M-1;j<N;j++) {
-        // count 배열 초기화 
-        let count = Array(MAX_LEN+1).fill(0)
+    for (let j=0;j<M;j++) {
+        tmp[j] = a[i+j]
+    }
 
-        for (let val of b) {
-            count[val] += 1
-        }
+    tmp = tmp.sort((a, b) => a - b)
+    // 같은 요소를 가진 배열인지 확인 
+    let isSame = true
+    for (let j=0;j<M;j++) {
 
-        // 잡은 구간에 b 수열 요소 있는지 판단 
-        for (let k=i;k<j+1;k++) {
-            count[a[k]] -= 1
+        if (tmp[j] !== b[j]) {
+            isSame = false
+            break
         }
-        
-        // 아름다운 수열인지 판단
-        let zeroCount = 0
-        
-        for (let v of count) {
-            if (v !== 0) break
-            
-            zeroCount += 1
-        }
+    }
 
-        if (zeroCount === MAX_LEN+1) {
-            answer += 1
-        }
+    if (isSame) {
+        answer += 1
     }
 }
 
