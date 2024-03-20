a88e693f21bd132666f3c48f3cd17712e8d7a94f
minjidev <68722909+minjidev@users.noreply.github.com>
2024-03-20 09:12:46 +0900

    Title: 특정 구간의 원소 평균값, Time: 197ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 240320/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.js
@@ -0,0 +1,28 @@
+// 구간의 시작점, 끝점 잡기 
+const fs = require('fs')
+const [n, input] = fs.readFileSync(0).toString().trim().split('\n')
+const N = +n
+const arr = input.split(' ').map(Number)
+let count = 0
+
+for (let i=0;i<N;i++) {
+    for (let j=i+1;j<N;j++) {
+        let sum = 0
+        // 평균 구하기 
+        for (let k=i;k<j+1;k++) {
+            sum += arr[k]
+        }
+        
+        const avg = Math.floor(sum/(j-i+1))
+
+        // 평균값 해당 구간에 포함되는지 확인
+        for (let k=i;k<j+1;k++) {
+            if (avg === arr[k]) {
+                count +=1 
+                break
+            } 
+        }
+    }
+}
+
+console.log(count)
