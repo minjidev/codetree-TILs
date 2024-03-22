afe25605395f896d024899a1212217637dd011d8
minjidev <68722909+minjidev@users.noreply.github.com>
2024-03-22 14:46:55 +0900

    Title: 그래프 탐색, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240322/그래프 탐색/graph-traversal.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const [n, ...input] = fs.readFileSync(0).toString().trim().split('\n')
+
+// 양방향 그래프 graph 만들기 
+// 1번에서 출발해 이동할 수 있으면 개수 세기 
+
+const [N, M] = n.split(' ').map(Number)
+const arr = input.map(row => row.split(' ').map(Number))
+
+const graph = Array.from({ length: N+1 }, () => [])
+const ch = Array(N+1).fill(0)
+let count = 0
+
+
+for (let [s, e] of arr) {
+    graph[s].push(e)
+    graph[e].push(s)
+}
+
+
+function DFS(L) {
+    for (let i=0;i<graph[L].length;i++) {
+        if (!ch[i]) {
+            ch[i] = 1
+            count += 1
+            DFS(L+1)
+        } 
+    }
+}
+
+DFS(1)
+console.log(count)
